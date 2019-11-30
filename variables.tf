b8ce1c77654d3e26990ccbdcf009d1b43b037f03
zerobig <zerobig.kim@gmail.com>
2019-11-30 11:08:30 +0900

    first commit

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "loc" {
+    description = "Default Azure region"
+    default     =   "koreacentral"
+}
+
+variable "tags" {
+    default     = {
+        source  = "citadel"
+        env     = "training"
+    }
+}
+
+variable "webapplocs" {
+    description = "List of locations for web apps"
+    type        = "list"
+    default     = []
+    
+}
